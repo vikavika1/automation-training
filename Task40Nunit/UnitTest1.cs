d7ff243d063beede926697d084cd09c45dfbb2b0
Viktoria Yaroshenko <viktoriayaroshenko@coherentsolutions.com>
2022-01-06 12:42:44 +0300

    1-4 Task50

--- Task40Nunit/UnitTest1.cs
+++ Task40Nunit/UnitTest1.cs
@@ -13,16 +13,21 @@ namespace Task40Nunit
         [SetUp]
         public void Setup()
         {
+
         }
 
         [Test]
-        public void Test1()
+        [TestCaseSource("LoginTestData")]
+        public void Test1(string username, string password)
         {
             IWebDriver driver = new ChromeDriver();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(4);
+
             driver.Navigate().GoToUrl("https://mail.ru/");
 
             var usernameBox = driver.FindElement(By.ClassName("email-input"));
-            usernameBox.SendKeys("vikavika234");
+            usernameBox.SendKeys(username);
 
             var domainBox = driver.FindElement(By.XPath("//option[@value='@inbox.ru']"));
             domainBox.Click();
@@ -31,16 +36,40 @@ namespace Task40Nunit
             enterForPasButton.Click();
 
             var inputPress = driver.FindElement(By.ClassName("password-input"));
+
+            // it is not waiter. Thread.Sleep used to suspend the execution of the current thread for a specified time in milliseconds
             Thread.Sleep(3000);
-            inputPress.SendKeys("polinka145b");
+            inputPress.SendKeys(password);
 
             var secondButton = driver.FindElement(By.ClassName("second-button"));
             secondButton.Click();
 
-            Thread.Sleep(5000);
-            Assert.IsTrue(driver.FindElement(By.CssSelector(".filters-control__filter-text")).Displayed, "Element Filter is not displayed");
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            var element = wait.Until(condition =>
+            {
+                try
+                {
+                  Assert.IsTrue(driver.FindElement(By.CssSelector(".filters-control__filter-text")).Displayed, "Element Filter is not displayed");
+                    return true;
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+
+            
 
             driver.Close();
         }
+        static object[] LoginTestData =
+        {
+           new object[] {"vikavika234", "polinka145b" },
+           new object[] {"vikavika546", "polinka145ggh" }
+        };
     }
 }
